b2c15635d2ef66e8d78f0390a0a2296fc99fbe81
Christopher Dempewolf <chris@chrisdempewolf.com>
2023-02-27 12:22:10 -0600

    Parse posts

--- content/index.php
+++ content/index.php
@@ -1,3 +1,22 @@
 <?php include '../includes/header.php' ?>
 <h1>Posts</h1>
+
+<?php
+$posts = array_diff(scandir('posts'), array('..', '.'));
+
+foreach ($posts as $k => $v) {
+    if (str_contains($v, ".ini")) {
+        continue;
+    }
+    $base_filename = str_replace(".php", "", $v);
+    $metadata = parse_ini_file("posts/" . $base_filename . ".ini");
+
+
+    $listing =  "<a href=\"posts/" . $base_filename . ".php\">" . $metadata['title'] . "</a>" . "<br>";
+    $listing = $metadata['creation_time'] . " - " . $listing;
+
+    echo $listing;
+}
+?>
+
 <?php include '../includes/footer.php' ?>
---
+++ content/posts/hello-again-world.php
@@ -0,0 +1,4 @@
+<?php include '../../includes/header.php' ?>
+<h1>Hello again, World!</h1>
+<p>Requisite and dutifully prosaic second post.  Is this thing still on?  Testing one... two... three.</p>
+<?php include '../../includes/footer.php' ?>
