8945e5a52ca307f286118e7907b9c2139be64f52
Christopher Dempewolf <chris@chrisdempewolf.com>
2023-02-27 11:34:30 -0600

    First post

---
+++ content/posts/0.php
@@ -0,0 +1,4 @@
+<?php include '../../includes/header.php' ?>
+    <h1>Hello, World!</h1>
+<p>Requisite and dutifully prosaic first post.  Is this thing on?  Testing one... two... three.</p>
+<?php include '../../includes/footer.php' ?>
