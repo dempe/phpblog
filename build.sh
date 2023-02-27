a7cbce7532900b7c20f1e8d97d1375f3f34cda72
Christopher Dempewolf <chris@chrisdempewolf.com>
2023-02-26 22:50:43 -0600

    Finish build script/add submodule

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+find ./output -type f ! -name '.git'
+rm -rf ./output/notes
 wget --directory-prefix=output/ -k -E -r -l 10 -p -N -F -nH http://0.0.0.0:2048
-for f in $(find ./output -type f); do mv "$f" $(echo $f | sed 's/\.php\.html/.html/'); done
-for f in $(find ./output -type f); do sed -i '' 's/\.php\.html/.html/g' $f; done
+for f in $(find ./output -type f ! -name '.git'); do mv "$f" $(echo $f | sed 's/\.php\.html/.html/'); done
+for f in $(find ./output -type f ! -name '.git'); do sed -i '' 's/\.php\.html/.html/g' "$f"; done
