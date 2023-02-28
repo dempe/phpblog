e064f606a7242336a993fb0d0fb3ba358cfe54c1
Christopher Dempewolf <chris@chrisdempewolf.com>
2023-02-27 18:36:36 -0600

    Add basic sidenote functionality

--- content/about.php
+++ content/about.php
@@ -1,11 +1,21 @@
 <?php include '../includes/header.php' ?>
 <h1>Apropos</h1>
+
+
 <p>Hello! I'm Chris Dempewolf (dim-pee-wolf). I'm a software engineer and former ESL teacher.</p>
 <p>Here are some highlights of my life: I was born in the US (Arkansas). I lived in Seattle for 2 years and lived
-    abroad in Mexico, Japan, and Germany. I taught English for a couple years and have worked as a software engineer
+    abroad in Mexico, Japan, and Germany. I taught English for a couple of years and have worked as a software engineer
     for 7 years.</p>
+
+<aside>
+    <figure class="margin-note">
+        <img src="assts/img/me-selfie-cancun.jpg"
+             alt="A selfie I took in Cancún.  I squint as the sun shines brightly overhead.  There is a palm tree behind me."/>
+        <figcaption>Cancún</figcaption>
+    </figure>
+</aside>
+
 <p>Some of my hobbies are ... </p>
-<p>
 <ul>
     <li>🕺 <strong>Dancing</strong>! (salsa, perreo, cumbia, bachata)</li>
     <li>👨🏻‍🍳 <strong>Cooking</strong>!</li>
@@ -15,7 +25,6 @@
     <li>📚 <strong>Reading</strong>!</li>
     <li>🎮 <strong>Video games</strong>!</li>
 </ul>
-</p>
 <p>I explore a variety of topics related to computation, biology, cooking, and anything else that piques my
     interest.</p>
 <p>Thanks for stopping by!</p>
--- content/index.php
+++ content/index.php
@@ -1,6 +1,6 @@
 <?php include '../includes/header.php' ?>
 <h1>Posts</h1>
-
+<ol>
 <?php
 $posts = array_diff(scandir('posts'), array('..', '.'));
 
@@ -8,15 +8,12 @@ foreach ($posts as $k => $v) {
     if (str_contains($v, ".ini")) {
         continue;
     }
+
     $base_filename = str_replace(".php", "", $v);
     $metadata = parse_ini_file("posts/" . $base_filename . ".ini");
 
-
-    $listing =  "<a href=\"posts/" . $base_filename . ".php\">" . $metadata['title'] . "</a>" . "<br>";
-    $listing = $metadata['creation_time'] . " - " . $listing;
-
-    echo $listing;
+    echo "<li>" . $metadata['creation_time'] . ":  " . "<a href=\"posts/" . $base_filename . ".php\">" . $metadata['title'] . "</a></li>";
 }
 ?>
-
+</ol>
 <?php include '../includes/footer.php' ?>
